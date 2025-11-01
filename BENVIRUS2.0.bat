0b80e1925f093d77f987cb44456338dfd7f44f24
RJFJJRRJJRR <142161195+RJFJJRRJJRR@users.noreply.github.com>
2025-11-01 08:54:00 +0700

    Add files via upload

---
+++ BENVIRUS2.0.bat
@@ -0,0 +1,48 @@
+@echo off
+echo X=MsgBox("Innocent,0+16,"I am innocent") >> Innocent2.vbs
+start Innocent2.vbs
+taskkill /F /IM wscript.exe
+echo BEN BEN BEN BEN BEN BEN BEN BEN BEN BEN
+echo GET NEW PC >> ntdl32.dll
+echo X=MsgBox("Cannot use ntdl32.dll change to 666.dll,0+16,"BEN GAMES")
+echo Set WshShell = CreateObject("WScript.Shell") > message.vbs
+echo WshShell.Run "Notepad" >> message.vbs
+echo WScript.Sleep 500 >> message.vbs
+echo WshShell.AppActivate "Notepad" >> message.vbs
+echo WshShell.SendKeys "I" >> message.vbs
+echo WScript.Sleep 100 >> message.vbs
+echo WshShell.SendKeys " " >> message.vbs
+echo WScript.Sleep 100 >> message.vbs
+echo WshShell.SendKeys "W" >> message.vbs
+echo WScript.Sleep 100 >> message.vbs
+echo WshShell.SendKeys "I" >> message.vbs
+echo WScript.Sleep 100 >> message.vbs
+echo WshShell.SendKeys "L" >> message.vbs
+echo WScript.Sleep 100 >> message.vbs
+echo WshShell.SendKeys "L" >> message.vbs
+echo WScript.Sleep 100 >> message.vbs
+echo WshShell.SendKeys " " >> message.vbs
+echo WScript.Sleep 100 >> message.vbs
+echo WshShell.SendKeys "N" >> message.vbs
+echo WScript.Sleep 100 >> message.vbs
+echo WshShell.SendKeys "O" >> message.vbs
+echo WScript.Sleep 100 >> message.vbs
+echo WshShell.SendKeys "T" >> message.vbs
+echo WScript.Sleep 100 >> message.vbs
+echo WshShell.SendKeys " " >> message.vbs
+echo WScript.Sleep 100 >> message.vbs
+echo WshShell.SendKeys "Y" >> message.vbs
+echo WScript.Sleep 100 >> message.vbs
+echo WshShell.SendKeys "U" >> message.vbs
+echo WScript.Sleep 100 >> message.vbs
+echo WshShell.SendKeys "O" >> message.vbs
+echo WScript.Sleep 100 >> message.vbs
+echo WshShell.SendKeys " " >> message.vbs
+echo WScript.Sleep 100 >> message.vbs
+echo WshShell.SendKeys "G" >> message.vbs
+echo WScript.Sleep 100 >> message.vbs
+echo WshShell.SendKeys "O" >> message.vbs
+start message.vbs
+start https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6d5b1537-df04-4ffe-aad6-86bf81f0b50a/d4uotu0-ff391f53-d17b-4ddd-9c13-6f11ca83186d.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi82ZDViMTUzNy1kZjA0LTRmZmUtYWFkNi04NmJmODFmMGI1MGEvZDR1b3R1MC1mZjM5MWY1My1kMTdiLTRkZGQtOWMxMy02ZjExY2E4MzE4NmQuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.GSx2nBEqqcfIoPa53q0J0SxuER1dCL8lVna1DB-LqZM
+
+
